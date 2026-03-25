57bb6399426cb5b0d850abb7e7423572b8c9b147
Brandon Lehr <brandonlehr@outlook.com>
2026-03-25 16:31:01 -0700

    Update Customer_landing.sql

--- SQL/Customer_landing.sql
+++ SQL/Customer_landing.sql
@@ -1,1 +1,19 @@
+CREATE EXTERNAL TABLE customer_landing (
+    serialnumber string,
+    sharewithpublicasofdate bigint,
+    birthday string,
+    registrationdate bigint,
+    sharewithresearchasofdate bigint,
+    customername string,
+    email string,
+    lastupdatedate bigint,
+    phone string,
+    sharewithfriendsasofdate bigint
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+LOCATION 's3://aws-glue-assets-495040113705-us-east-1/customer_landing/';
 
+
+
+#Validation query
+SELECT COUNT(*) FROM customer_landing;
