c90df8923581761579793aabf24a1be85504c6d0
Brandon Lehr <brandonlehr@outlook.com>
2026-03-25 16:33:19 -0700

    Update step_trainer_landing.sql

--- SQL/step_trainer_landing.sql
+++ SQL/step_trainer_landing.sql
@@ -1,1 +1,10 @@
+CREATE EXTERNAL TABLE step_trainer_landing (
+    sensorreadingtime bigint,
+    serialnumber string,
+    distancefromobject int
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+LOCATION 's3://aws-glue-assets-495040113705-us-east-1/step_trainer_landing/';
 
+#Validation query
+SELECT COUNT(*) FROM step_trainer_landing;
