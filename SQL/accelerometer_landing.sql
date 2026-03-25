876b0aa2a9deb0483168af737b74acf5e65a3144
Brandon Lehr <brandonlehr@outlook.com>
2026-03-25 16:32:27 -0700

    Update accelerometer_landing.sql

--- SQL/accelerometer_landing.sql
+++ SQL/accelerometer_landing.sql
@@ -1,1 +1,13 @@
+CREATE EXTERNAL TABLE accelerometer_landing (
+    timestamp bigint,
+    user string,
+    x double,
+    y double,
+    z double
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+LOCATION 's3://aws-glue-assets-495040113705-us-east-1/accelerometer_landing/';
 
+
+#Validation query
+SELECT COUNT(*) FROM accelerometer_landing;
