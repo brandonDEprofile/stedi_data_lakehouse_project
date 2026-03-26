96b6de7525edd96064e8847c03b8da40d513f36b
Brandon Lehr <brandonlehr@outlook.com>
2026-03-25 21:14:56 -0700

    Update accelerometer_landing_to_trusted.py

--- glue_jobs/accelerometer_landing_to_trusted.py
+++ glue_jobs/accelerometer_landing_to_trusted.py
@@ -27,19 +27,24 @@ DEFAULT_DATA_QUALITY_RULESET = """
 """
 
 # Script generated for node AWS Glue Data Catalog
-AWSGlueDataCatalog_node1774472741544 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="customer_landing", transformation_ctx="AWSGlueDataCatalog_node1774472741544")
+AWSGlueDataCatalog_node1774473316891 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="accelerometer_landing", transformation_ctx="AWSGlueDataCatalog_node1774473316891")
+
+# Script generated for node AWS Glue Data Catalog
+AWSGlueDataCatalog_node1774474495255 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="customer_trusted", transformation_ctx="AWSGlueDataCatalog_node1774474495255")
 
 # Script generated for node SQL Query
 SqlQuery0 = '''
-select * from myDataSource
-WHERE sharewithresearchasofdate IS NOT NULL
+SELECT a.*
+FROM accelerometer_landing a
+JOIN customer_trusted c
+ON a.user = c.email
 '''
-SQLQuery_node1774472796131 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"myDataSource":AWSGlueDataCatalog_node1774472741544}, transformation_ctx = "SQLQuery_node1774472796131")
+SQLQuery_node1774474702488 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"accelerometer_landing":AWSGlueDataCatalog_node1774473316891, "customer_trusted":AWSGlueDataCatalog_node1774474495255}, transformation_ctx = "SQLQuery_node1774474702488")
 
 # Script generated for node Amazon S3
-EvaluateDataQuality().process_rows(frame=SQLQuery_node1774472796131, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1774472674578", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
-AmazonS3_node1774472819726 = glueContext.getSink(path="s3://aws-glue-assets-495040113705-us-east-1/customer_trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="AmazonS3_node1774472819726")
-AmazonS3_node1774472819726.setCatalogInfo(catalogDatabase="stedi",catalogTableName="customer_trusted")
-AmazonS3_node1774472819726.setFormat("json")
-AmazonS3_node1774472819726.writeFrame(SQLQuery_node1774472796131)
+EvaluateDataQuality().process_rows(frame=SQLQuery_node1774474702488, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1774474478906", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+AmazonS3_node1774474856252 = glueContext.getSink(path="s3://aws-glue-assets-495040113705-us-east-1/accelerometer_trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="AmazonS3_node1774474856252")
+AmazonS3_node1774474856252.setCatalogInfo(catalogDatabase="stedi",catalogTableName="accelerometer_trusted")
+AmazonS3_node1774474856252.setFormat("json")
+AmazonS3_node1774474856252.writeFrame(SQLQuery_node1774474702488)
 job.commit()
